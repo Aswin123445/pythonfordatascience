d1fe94de22a6e7f8890685cb1cc1d6d6cdce59f3
java_learing <aswinsandeep4@gmail.com>
2024-07-04 10:16:41 +0530

    adding python files

---
+++ basicpython/FIrst.py
@@ -0,0 +1,11 @@
+#first program to print
+print("hi")
+#code to input text
+input("Enter your name")
+#using variables  and using concept of concatination for outputing
+name=input("enter your name")
+print("helo "+name)
+print("helo",end="")
+print(" aswin")
+
+
---
+++ basicpython/level2.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+a=10
+b=20
+sumOfaAndb=a+b
+
+
+# In[5]:
+
+
+print(sumOfaAndb)
+
+
+# In[24]:
+
+
+type(sumOfaAndb)
+
+
+# In[7]:
+
+
+type(a+b)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[8]:
+
+
+c=7.4
+type(a+c)
+
+
+# In[11]:
+
+
+#type is float because it is upcasted
+
+
+# In[12]:
+
+
+s1="hello"
+s2="world"
+print(s1+s2)
+
+
+# In[13]:
+
+
+#boolean calulation
+get_ipython().run_line_magic('whos', '')
+
+
+# In[14]:
+
+
+print(a and b)
+
+
+# In[16]:
+
+
+a=True
+b=False
+print(a and b)
+print(a or b)
+
+
+# In[17]:
+
+
+print(not(a))
+
+
+# In[18]:
+
+
+get_ipython().run_line_magic('whos', '')
+
+
+# In[20]:
+
+
+print(round(4.556))
+
+
+# In[22]:
+
+
+print(round(4.445))
+
+
+# In[23]:
+
+
+print(round(4.55534,2))
+
+
+# In[ ]:
+
+
+
+
