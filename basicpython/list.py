0216119e4cebb68e2077b6c41342e8d5930bc527
java_learing <aswinsandeep4@gmail.com>
2024-07-07 14:48:52 +0530

    add python tutorial files

---
+++ basicpython/list.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#used to group together other values
+prime=[2,3,5,7]
+
+
+# In[4]:
+
+
+prime
+
+
+# In[5]:
+
+
+prime[1]
+
+
+# In[6]:
+
+
+#indexing from the end
+prime[-1]
+
+
+# In[7]:
+
+
+#list also support concatination of items
+prime +[10,30]
+
+
+# In[8]:
+
+
+#list are mutable
+prime[3]=5
+prime
+
+
+# In[9]:
+
+
+#append funtion is used to add new items at the end of the list
+prime.append(89)
+
+
+# In[10]:
+
+
+prime
+
+
+# In[11]:
+
+
+#finding the length of the list
+len(prime)
+
+
+# In[ ]:
+
+
+
+
